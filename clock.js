4b9d301eab828036380ed55e980e6e038fe52c32
Aryan Naimi <aryannaimi@gmail.com>
2018-06-26 18:07:06 -0700

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,29 @@
+var $ = function(id) { return document.getElementById(id); };
+
+var displayCurrentTime = function() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    t = setTimeout(function() {
+        displayCurrentTime()
+    }, 500);
+
+    var ampm = "AM";
+    if (h > 12) {
+        h = h - 12;
+        ampm = "PM";
+    }
+    document.getElementById("hours").innerHTML = padSingleDigit(h);
+    document.getElementById("minutes").innerHTML = padSingleDigit(m);
+    document.getElementById("seconds").innerHTML = padSingleDigit(s);
+    document.getElementById("ampm").innerHTML = ampm;
+};
+
+var padSingleDigit = function(num) {
+    return (num < 10) ? "0" + num:num;
+};
+
+window.onload = function() {
+    displayCurrentTime();
+};
